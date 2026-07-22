16f5a122a372c0496b7cd35af07505bfa7c51e50
özgür kavak <ozgurkvk15@gmail.com>
2026-07-22 22:32:08 +0300

    Öğrenci listeleme özligi eklendi

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from menu import baslik_goster,menu_goster,kullanici_secimi
+from ogrenci import ogrenci_ekle,ogrencileri_listele
 
 baslik_goster()
 menu_goster()
 
 secim=kullanici_secimi()
-print(f"seçiminiz:{secim}")
+print(f"Seçiminiz: {secim}")
+
+while True:
+    menu_goster()
+
+    secim = kullanici_secimi()
+
+    if secim == "1":
+        ogrenci_ekle()
+
+    elif secim == "2":
+        ogrencileri_listele()
+
+    elif secim == "7":
+        print("Program kapatılıyor...")
+        break
