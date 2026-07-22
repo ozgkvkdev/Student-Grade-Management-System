f9e584ccd89548b15440b0e811351e0fd73c1bac
özgür kavak <ozgurkvk15@gmail.com>
2026-07-22 21:32:48 +0300

    ana menu sistemi oluşturuldu

---
+++ menu.py
@@ -0,0 +1,17 @@
+# sistem mesajı verildi
+print("="*50)
+print("ÖĞRENCİ NOT YÖNETİM SİSTEMİ")
+print("="*50)
+
+# seçim ekranı
+
+def menu_göster():
+    print("1- Öğrenci Ekle")
+    print("2- Öğrencileri Listele")
+    print("3- Not Ekle")
+    print("4- Ortalama Hesapla")
+    print("5- Öğrenci Ara")
+    print("6- Öğrenci Sil")
+    print("7- çıkış")
+
+menu_göster()
